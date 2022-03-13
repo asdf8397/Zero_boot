41a03f8f05d354ded6a0d95412866bc9ed2be912
asdf8397 <scv2314@naver.com>
2022-03-13 23:13:09 +0900

    2022.03.13

--- JavaScript기본/값/3.참조타입.js
+++ JavaScript기본/값/3.참조타입.js
@@ -3,15 +3,3 @@
  * -- 원시(Primitive)
  * -- 객체, 참조(Reference)
  */
-
-const object = {};
-const array = [];
-function func() {}
-
-console.log(object instanceof object);
-console.log(func instanceof Function);
-console.log(array instanceof Array);
-
-console.log(Object.prototype.toString(Array));
-console.log(Object.prototype.toString(Array));
-console.log(Object.prototype.toString(Array));

e116e8dfcf118f141506e02032aa6f1856bb50a3
asdf8397 <scv2314@naver.com>
2022-06-16 11:04:09 +0900

    2022.06.16 React_study start

---
+++ React 이론/리액트에 대한 이해/2.프론트엔드 제어를 위한 JS와 리액트코드 비교.js
@@ -0,0 +1,10 @@
+const dataMap = {
+    메일: "메일함을 확인하세요.",
+    카페: "즐겨찾는 카페의 새 소식을 확인해보세요.",
+    블로그: "오늘을 기록해보세요."
+};
+
+function clickTabItem(e) {
+    const key = e.target.innerHTML;
+    const contentEl = document.querySelector(".")
+}
